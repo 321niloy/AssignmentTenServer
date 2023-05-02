f00ef87b1f4b6a96fa2c8293135abc2978a0a65b
321niloy <niloyn670@gmail.com>
2023-05-02 23:47:36 +0600

    recipecards json start

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const port = 9000
-
+const card  = require('./data/cards.json')
 
 app.use(cors())
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/card', (req, res) => {
+  res.send(card)
 })
 
 app.listen(port, () => {
